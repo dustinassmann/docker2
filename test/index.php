d78c8ad22203bce89f7d1597941980e110a5b1f5
d.assmann <d.assmann@ks-gebaeudetechnik.de>
2020-08-27 15:46:43 +0200

    Update index.php

--- test/index.php
+++ test/index.php
@@ -7,14 +7,14 @@ $dbuser = 'QuitScope2';
 $dbpass = 'Kd101301.';
 $dbhost = '127.0.0.1';
 
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
 $test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
+$result = mysqli_query($link, $test_query);
 
 $tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
+while($tbl = mysqli_fetch_array($result)) {
   $tblCnt++;
   #echo $tbl[0]."<br />\n";
 }
